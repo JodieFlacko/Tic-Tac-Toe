474612066472c06fa400f120aba733851eb0100d
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-02-01 19:58:56 +0100

    feat: fetching data
    It is possible to fetch players from console.

---
+++ script.js
@@ -0,0 +1,62 @@
+/* const gameController = (function(){
+
+    init = (function(){
+        this.gameboard = this.createBoard();
+    })(),
+
+    createBoard = function(){
+        const rows = 3;
+        const columns = 3;
+        const board = [[], []];
+
+        return {rows, columns, board};
+    },
+
+    printBoard = function(){
+        let board = this.gameboard.board;
+        for(let rows = 0; rows < board.rows; rows++){
+            for(let columns = 0; columns < board.columns; columns++){
+                board[[rows][columns]] = rows + columns;  
+                console.log(board.board[[rows][columns]] + " ");
+            }
+            console.log("Fratm");
+        }
+    }
+
+    
+})() */
+
+gameboard = {
+    rows: 3,
+    columns: 3,
+    board: [],
+};
+
+const gameController = (function(){
+    const createPlayer = function(marker = undefined){
+        // if prompting second player
+        let score = 0;
+        if(marker !== undefined){
+            const name = prompt('Enter second player name: ');
+            marker = marker === 'X' ? "O" : "X";
+            return {name, marker}
+        }
+        const name = prompt('Enter first player name: ');
+        let signal = true;
+        while(signal){
+            marker = prompt('Enter a marker. Choose between "X" and "O": ')
+            if(marker === "X" || marker === "O") signal = false;
+        }
+        return {name, marker, score}
+    };
+
+    const getPlayers = function(){
+            playerOne = createPlayer();
+            playerTwo = createPlayer(playerOne.marker);
+            return {playerOne, playerTwo}
+    }
+    
+
+    return { getPlayers }
+})();
+
