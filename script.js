854ff108bfc93ce9a80d229af62a0bdb21cc6aab
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-02-02 17:50:15 +0100

    feat: user see gameboard updating

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const gameController = (function(){
         return {name, marker, score}
     };
 
-    const getPlayers = function(){
+    function getPlayers(){
             playerOne = createPlayer();
             playerTwo = createPlayer(playerOne.marker);
             return { playerOne, playerTwo }
@@ -55,49 +55,72 @@ const gameController = (function(){
     gameboard = [];
 
 
-    const startGame = function(){
+    function startGame(){
         //private variable
         players = getPlayers();
         const showPlayers = (function(){
         console.log(`First player\n Name: ${players.playerOne.name} - Marker: ${players.playerOne.marker} - Score: ${players.playerOne.score}\nSecond player\n Name: ${players.playerTwo.name} - Marker: ${players.playerTwo.marker} - Score: ${players.playerTwo.score}\n`);
         })(players);
 
-        const checkWinner = function(signal){
-            if(gameboard[0] === "X" && gameboard[1] === "X" && gameboard[2] === "X") {
-                signal.value = false;
-                return true;
-            }
-        }
-
-        const promptForPosition = function(player){
-            alert(`${players[player].name}'s Turn:\n`)
-            const row = prompt(`Enter the ROW where you want to put the marker: `);
-            const column = prompt(`Enter the COLUMN where you want to put the marker: `);
-            let position = row * column - 1;
-            return position;
-        }
-
-        const updateScore = function(winner){
-            winner.score++;
-        }
-
         const insertMarker = (function(){
-            let signal = {
-                value: true,
-            }
-            while(signal.value){
+            //allowing modification passing the reference
+            while(true){
                 for(let player in players){
                     const position = promptForPosition(player);
                     gameboard[position] = players[player].marker;
-                    if(checkWinner(signal)){
-                        const winner = players[player];
-                        updateScore(winner);
-                        console.log(players[player])
-                        break;
+                    printBoard();
+                    if(checkWinner()){
+                        updateScore(players[player]);
+                        console.log("Winner is: " + players[player].name);
+                        return;
                     }
                 }
             }
-        })(players, gameboard, checkWinner, promptForPosition, updateScore);
+        })(players, gameboard);
+
+        function checkWinner(signal){
+            winningLines = {
+                firstRow: [0, 1, 2],
+                secondRow: [3, 4, 5],
+                thirdRow: [6, 7, 8],
+                firstCol: [0, 3, 6],
+                secondCol: [1, 4 , 7],
+                thirdCol: [2, 5, 8],
+                firstDiag: [0, 4, 8],
+                secondDiag: [2, 4, 6],
+            };
+            
+
+            for(line in winningLines){
+                let keepGoing = true;
+                winningLines[line].forEach(index => {
+                    if(gameboard[index] !== "X") {
+                        keepGoing = false;
+                        return;
+                    }  
+                });
+                if(keepGoing) {
+                    return true;
+                };
+            }
+        }
+
+        function promptForPosition(player){
+            alert(`${players[player].name}'s Turn:\n`)
+            const row = prompt(`Enter the ROW where you want to put the marker: `);
+            const col = prompt(`Enter the COLUMN where you want to put the marker: `);
+            let position = (+row - 1) * 3 + +col - 1;
+            return position;
+        }
+
+        function updateScore(winner){
+            winner.score++;
+        }
+
+        function printBoard(){
+            for(let pos = 0, add = 0; pos < 3; pos++, add += 2)
+            console.log(`${gameboard[pos + add] || " "} | ${gameboard[pos + 1 + add] || " "} | ${gameboard[pos + 2 + add] || " "}\n`);
+        }
     }
 
     return { startGame }
