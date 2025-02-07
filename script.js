a58a3e8d986a4af0f1abebf36caa298fff1fbfc5
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-02-07 15:37:21 +0100

    feat: clear board if a user win or there is a tie.
    Clear board if user clicks on it after a user wins or there is a tie.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function Gameboard(){
     let board = [];
     const rows = 3;
     const columns = 3;
+    let status;
     //This loop creates a bidimensionale array of cells (later defined by Cell())
     for(let i = 0; i < rows; i++){
         board[i] = [];
@@ -13,6 +14,7 @@ function Gameboard(){
 
     //For later UI use
     const getBoard = () => board;
+    const getStatus = () => status;
     const printBoard = () => {
         const boardWithCellMarks = board.map((row) => row.map(cell => cell.getValue()));
         console.log(boardWithCellMarks);
@@ -25,21 +27,33 @@ function Gameboard(){
         board[row][column].updateMark(player);
     }
 
-    const hasWinner = (row, column, player) => {
+    const updateStatus = (row, column, player) => {
         const rowStreak = board[row].filter(cell => cell.getValue() === player);
         const columnStreak = board.filter(cell => cell[column].getValue() === player);
+        const emptyCells = board.filter(row => row.filter(cell => cell.getValue() === "").length !== 0);
         let diagonalStreak;
         if((player === board[0][0].getValue() && player === board[1][1].getValue() && player === board[2][2].getValue()) || (player === board[0][2].getValue() && player === board[1][1].getValue() && player === board[2][0].getValue())) diagonalStreak = true;
 
-        if(rowStreak.length === 3 || columnStreak.length === 3 || diagonalStreak) return true;
+        if(rowStreak.length === 3 || columnStreak.length === 3 || diagonalStreak) {
+            status = "win";
+        }
+
+        else if(!emptyCells.length) {
+            status = "tie";
+        }
     };
 
-    const hasTie = () =>{
-        const emptyCells = board.filter(row => row.filter(cell => cell.getValue() === "").length !== 0);
-        if(!emptyCells.length) return true;
-    };
+    const clearBoard = () =>{
+        for(let i = 0; i < rows; i++){
+            board[i] = [];
+            for(let j = 0; j < columns; j++){
+                board[i].push(cell());
+            }
+        }
+        status = ''; 
+    }
 
-    return { getBoard, printBoard, addMark, hasWinner, hasTie };
+    return { getBoard, printBoard, addMark, getStatus, clearBoard, updateStatus};
 }
 
 function cell(){
@@ -56,15 +70,16 @@ function GameController(){
         {
             name: "playerOne",
             mark: "X",
+            score: 0,
         },
         {
             name: "playerTwo",
             mark: "O",
+            score: 0,
         },
     ];
 
     const board = Gameboard();
-
     let activePlayer = players[0]; 
 
     const switchPlayerTurn = () => {
@@ -77,18 +92,22 @@ function GameController(){
         console.log(`${getActivePlayer().name}'s turn.`);
     }
 
+    const updateScore = () => {
+        activePlayer.score++;
+    }
+
+
+
     const playRound = (row, column) => {
+
         activePlayer = getActivePlayer();
+
         console.log(`${activePlayer.name} mark with ${activePlayer.mark} row: ${row}, column: ${column}`);
         board.addMark(row,column, activePlayer.mark);
-
-
-        if(board.hasWinner(row, column, activePlayer.mark)){
-            console.log(`Winner is ${activePlayer.name}`);
-            return;
-        } 
-        if(board.hasTie()){
-            console.log('Tie');
+        board.updateStatus(row, column, activePlayer.mark)
+        boardStatus = board.getStatus();
+        if(boardStatus === "win") {
+            updateScore();
             return;
         }
 
@@ -99,35 +118,33 @@ function GameController(){
     //initial game message
     printNewRound();
 
-    return { playRound, getActivePlayer, getBoard: board.getBoard };
+    return { playRound, getActivePlayer, getBoard: board.getBoard, getBoardStatus: board.getStatus, clearBoard: board.clearBoard };
 }
 
 function screenController(){
     const game = GameController();
-
     const DOMelements = (function cacheDOM(){
-        boardDiv = document.querySelector(".game");
-        turnDiv = document.querySelector(".turn");
-
-        return { boardDiv, turnDiv };
+        const boardDiv = document.querySelector(".game");
+        const turnDiv = document.querySelector(".turn");
+        const boardRows = boardDiv.querySelectorAll(".row");
+        const cells = boardDiv.querySelectorAll(".row div");
+        return { boardDiv, turnDiv, boardRows, cells };
     }());
 
-    function updateSreen(){
+    function updateSreen(){        
         //get board
         const board = game.getBoard();
-        const boardRows = boardDiv.querySelectorAll(".row");
-        const activePlayer = game.getActivePlayer();
-
+        
 
         //fill the cells
-        boardRows.forEach((row, index) => {
+        DOMelements.boardRows.forEach((row, index) => {
             const rowIndex = index;
             const boardColumns = row.querySelectorAll(".cell");
             boardColumns.forEach((buttonCell, index) =>{
                 const cellMark = buttonCell.querySelector("div");
                 buttonCell.dataset.row = rowIndex;
                 buttonCell.dataset.column = index;
-                if(!cellMark.classList.contains("x") && !cellMark.classList.contains("o")){
+                if(!cellMark.hasAttribute("class")){
                     if(board[rowIndex][index].getValue() === "X") cellMark.classList.add("x");
                     else if(board[rowIndex][index].getValue() === "O") cellMark.classList.add("o");
                 }
@@ -137,6 +154,16 @@ function screenController(){
 
     //event handler
     function clickHandler(event) {
+        //get board status
+        const boardStatus = game.getBoardStatus();
+        //if someone wins or there is a tie the board is cleared
+        if(boardStatus === "win" || boardStatus === "tie"){
+            DOMelements.cells.forEach(cell => cell.removeAttribute("class"));
+            game.clearBoard();
+            return;
+        }
+
+        //else a mark is inserted
         const selectedRow = event.target.dataset.row;
         const selectedColumn = event.target.dataset.column;
         
